473333c36e63d5e7e6ec8a9c526766716549fc71
Hemant Kumar <hemant.kumar@hemantsss327.successivetech.com>
2023-01-22 18:55:59 +0530

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        foreach( range(1,20) as $index) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(5).'@mail.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
+    }
+}
